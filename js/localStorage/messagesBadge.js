51c17f4e4a03f89b778bb9e8182ea930bd9be89c
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-11-14 19:17:43 -0300

    refactor: alteração da estrutura que os dados são armazenados no localStorage

--- js/localStorage/messagesBadge.js
+++ js/localStorage/messagesBadge.js
@@ -1,9 +1,11 @@
 const badge = document.getElementById('message-badge');
 
 function messagesBagde() {
-    if(localStorage.length > 0) {
-        badge.style.display = "block"
-        badge.innerText = localStorage.length;
+    const messageList = localStorage.getItem('messages');
+    const jsonMessageList = JSON.parse(messageList);
+    if(jsonMessageList.length > 0) {
+        badge.style.display = "block";
+        badge.innerText = jsonMessageList.length;
     };
 };
 
--- js/localStorage/messagesForm.js
+++ js/localStorage/messagesForm.js
@@ -9,16 +9,37 @@ const contactForm = document.getElementById('contact-form');
 
 // Form Submit Event
 contactForm.addEventListener('submit', _ => {
-    const formData = {
-        name: name.value,
-        email: email.value,
-        subject: subject.value,
-        message: message.value
+    if(localStorage.getItem('messages')) {
+        // adiciona item a lista já existente de mensagens
+        const messageList = localStorage.getItem('messages');
+        const jsonMessageList = JSON.parse(messageList);
+        const lastId = jsonMessageList[jsonMessageList.length - 1].id;
+
+        const formData = {
+            id: lastId + 1,
+            name: name.value,
+            email: email.value,
+            subject: subject.value,
+            message: message.value
+        };
+
+        jsonMessageList.push(formData);
+
+        localStorage.setItem('messages', JSON.stringify(jsonMessageList));
+        contactForm.reset();
+    } else { 
+        // cria uma nova lista de mensagens
+        const formData = {
+            id: 1,
+            name: name.value,
+            email: email.value,
+            subject: subject.value,
+            message: message.value
+        };
+
+        const messageList = [formData];
+
+        localStorage.setItem('messages', JSON.stringify(messageList));
+        contactForm.reset();
     };
-
-    const JsonFormData = JSON.stringify(formData);
-
-    localStorage.setItem(email.value, JsonFormData);
-
-    contactForm.reset();
 });
