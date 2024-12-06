1084406193a9a4208eae189687694fd63f90bf8a
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-12-06 20:44:27 -0300

    feat(cliente): adicionando suporte para contas e inicialização de endereços no construtor

--- src/models/Cliente.ts
+++ src/models/Cliente.ts
@@ -1,16 +1,20 @@
 import { IUsuario } from "../interfaces/IUsuário";
+import { Conta } from "./conta/Conta";
 import { Endereco } from "./Endereco";
 import { Pessoa } from "./Pessoa";
 
 export class Cliente extends Pessoa implements IUsuario {
     private _vip: boolean;
-    private _enderecos: Endereco[] = [];
+    private _enderecos: Endereco[];
     private _autenticado: boolean;
+    private _contas: Conta[];
 
-    constructor(_nome: string, _cpf: string, _telefone: string){
+    constructor(_nome: string, _cpf: string, _telefone: string, _cep: string, _logradouro: string, _numero: string, _complemento: string, _cidade: string, _estado: string){
         super(_nome, _cpf, _telefone);
+        this._enderecos = [new Endereco(1, _cep, _logradouro, _numero, _complemento, _cidade, _estado)];
         this._vip = false;
         this._autenticado = false;
+        this._contas = []
     };
 
     // Getters
@@ -51,5 +55,9 @@ export class Cliente extends Pessoa implements IUsuario {
     public autenticar(): boolean {
         this._autenticado = true;
         return true;
+    };
+
+    public adicionarConta(conta: Conta) {
+        this._contas.push(conta);
     };
 };
